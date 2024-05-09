26f39238c395612cd58045fdee46c4b9dc740a8f
Yolberto Mastrapa <yolber19@gmail.com>
2024-05-08 20:44:43 -0400

    Timestamp Microservice

--- index.js
+++ index.js
@@ -30,3 +30,14 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/:date?", (req, res, next)=>{
+  req.date = {
+    "unix": new Date().getTime(),
+    "utc": new Date().toUTCString()
+  };
+  next();
+},function(req, res){
+  res.send(req.date)
+}
+)
