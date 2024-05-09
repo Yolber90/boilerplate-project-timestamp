9c162f16fadc2cb4eb5f79f7c13f27117539fefe
Yolberto Mastrapa <yolber19@gmail.com>
2024-05-08 22:55:40 -0400

    getting thre

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,44 +21,67 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+// HERE*********************************
+app.get("/api/:date?", (req, res) => {
+  //params gets the URL value
+  const urlValue = req.params.date;
+  
+  let check = /[\d-]{5,}/.test(urlValue)
+
+  const analyzeUrlDate = (dt) => {
+    if(/^[1]/.test(dt) !== true){
+      let stringDate = urlValue.toString()
+      const unixDate = new Date(stringDate).getTime()
+      const utcDate = new Date(stringDate).toUTCString()
+      return res.send(unixDate + " - " + utcDate)
+    }else{
+      const unixDate = new Date(parseInt(urlValue)).getTime()
+      const utcDate = new Date(parseInt(urlValue)).toUTCString()
+      return res.send(unixDate + " - " + utcDate)
+    }
+  }
+
+
+
+
+  res.send(analyzeUrlDate(urlValue))
+
+  // res.json(check)
+
+  // if (check == true) {
+  //   res.json({
+  //     "unix": unixDate,
+  //     "utc": utcDate
+  //   })
+  // }
+  
+
+  // if(check == true){
+  //   res.json({
+  //     "unix": unixDate
+  //   })
+  // }
+
+
+  // const realDate = new Date(urlValue).toUTCString()
+
+  // res.json({ "date": check})
+
+})
+// HERE*********************************
 
 // app.get("/api/:date?", (req, res)=>{
 //   req.date = {
 //     "unix": new Date().getTime(),
 //     "utc": new Date().toUTCString()
 //   };
-//   res.json(req.date)
+//   res.send(req.date)
 // }
 // )
 
-app.get("/api/:date?", (req, res) => {
-  // Extract date parameter from request 
-  const dateParam = req.params.date; 
-  // Initialize variable to hold date object
-   let dateObject; 
-  // if no date parameter is provided, use current date
-  if (!dateParam) {
-    dateObject = new Date(); 
-  }
-  // if parameter is in unix timestamp format(5 or more digits), then convert it to a date object
-  else if (/\d{5,}/.test(dateParam)) {
-    dateObject = new Date(parseInt(dateParam)); 
-  }
-  // if date parameter in a string, then convert it to  a date object 
-  else {
-    dateObject = new Date(dateParam); 
-  }
-  // if date object is invalid, return a error response ({ error: "Invalid Date" })
-  if (isNaN(dateObject)) {
-    return res.json({ error: "Invalid Date" }); 
-  }
-  // if date is valid, return unix timestamp and utc string
-  else {
-    return res.json({unix: dateObject.getTime(), utc: dateObject.toUTCString()}); 
-  }
-});
 
 
 // Listen on port set in environment variable or default to 3000
---
+++ test.js
@@ -0,0 +1,6 @@
+let timeTest = new Date("2015-12-25").getTime()
+console.log(timeTest);
+
+let check = /^[1]/
+
+console.log(check.test(15146456))
